d26821c772872d7cc705277bdd3c91c8fb74677a
Brenton Bull <bbull@savagebull.com.au>
2017-09-07 16:08:34 +1000

    Channel and blog updates.

--- gulpfile.js
+++ gulpfile.js
@@ -71,7 +71,7 @@ gulp.task('sass', function () {
     return gulp.src(['./static/css/main.scss'])
         .pipe(sourcemaps.init())
         .pipe(sass({
-            includePaths: ['./static/css/partials']
+            outputStyle: 'nested' // Options: nested, expanded, compact, compressed
         }).on('error', sass.logError))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 10', 'opera 12.1', 'ios 6', 'android 4'))
         .pipe(sourcemaps.write('.'))
